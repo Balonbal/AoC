541319f402103c95d45de274dbb3ae7006c767e8
Olav Bech Bråten <olavbb@hotmail.com>
2016-12-08 14:10:52 +0100

    Day 3 part 2

--- src/org/gabeorama/day3/Day3.java
+++ src/org/gabeorama/day3/Day3.java
@@ -21,4 +21,30 @@ public class Day3 {
 
         System.out.printf("# of possible triangles: %d", p);
     }
+
+    public static void run2(String input) {
+        int[][] triplet = new int[3][3];
+        int p = 0;
+
+        String[] lines = input.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            String[] integers = lines[i].trim().split("(\\s+)");
+
+            if (i % 3 == 0) triplet = new int[3][3];
+
+            for (int j = 0; j < integers.length; j++) {
+                triplet[j][i % 3] = Integer.parseInt(integers[j]);
+            }
+            
+            if (i % 3 == 2) {
+                for (int[] col: triplet) {
+                    int max = Math.max(col[0], Math.max(col[1], col[2]));
+
+                    if (max < col[0] + col[1] + col[2] - max) p++;
+                }
+            }
+        }
+
+        System.out.printf("# of possible triangles: %d", p);
+    }
 }
