a2a4b002d8a99f49b91cdb589a4dc387db463a72
Olav Bech Bråten <olavbb@hotmail.com>
2016-12-08 03:38:20 +0100

    Completed day 1 part 2

--- src/org/gabeorama/day1/Day1.java
+++ src/org/gabeorama/day1/Day1.java
@@ -1,5 +1,10 @@
 package org.gabeorama.day1;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Sly on 08.12.2016.
  */
@@ -36,5 +41,46 @@ public class Day1 {
         }
 
         System.out.printf("Movement is %d", Math.abs(x) + Math.abs(y));
+    }
+
+    public static void run2(String input) {
+        int x = 0, y = 0;
+        int direction = 0; //N = 0, E = 1, S = 2, W = 3
+        Set<Point> locations = new HashSet<>();
+        locations.add(new Point(x, y));
+        String[] dirs = input.split(", ");
+        for (int i = 0; i < dirs.length; i++) {
+            switch(dirs[i].substring(0, 1)) {
+                case "R":
+                    //Turn and resolve for a full turn
+                    if (++direction > 3) {
+                        direction = 0;
+                    }
+                    break;
+                case "L":
+                    //Same as above
+                    if (--direction < 0) {
+                        direction = 3;
+                    }
+                    break;
+            }
+            int mult = (direction > 1 ? -1 : 1); //Positive or negative movement
+            int movement = Integer.parseInt(dirs[i].substring(1));
+
+            for (int j = 0; j < movement; j++) {
+                if (direction % 2 == 0) { //N or S
+                    y += mult;
+                } else {
+                    x += mult;
+                }
+                Point location = new Point(x, y);
+                if (locations.contains(location)) {
+                    System.out.printf("Location found at %d blocks away\n", Math.abs(x) + Math.abs(y));
+                }
+                locations.add(location);
+            }
+
+        }
+
     }
 }
